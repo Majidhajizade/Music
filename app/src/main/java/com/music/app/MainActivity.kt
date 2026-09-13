51aa60d037e3e4f2e3344ae9e040cab1469e3093
Majid Hajizade <majidhajizadehbarzi@gmail.com>
2026-09-13 13:53:38 +0330

    Hide status bar in now playing

--- app/src/main/java/com/music/app/MainActivity.kt
+++ app/src/main/java/com/music/app/MainActivity.kt
@@ -3299,6 +3299,12 @@ class MainActivity : ComponentActivity() {
             window.statusBarColor = Color.TRANSPARENT
             window.navigationBarColor = Color.TRANSPARENT
 
+            window.decorView.systemUiVisibility =
+                android.view.View.SYSTEM_UI_FLAG_FULLSCREEN or
+                android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
             if (android.os.Build.VERSION.SDK_INT >= 29) {
                 window.isStatusBarContrastEnforced = false
                 window.isNavigationBarContrastEnforced = false
